e4cda9e1c5f84bf9d01a467b9ef10349c64bd76f
brettpo <48755473+brettpo@users.noreply.github.com>
2023-05-05 09:42:07 +0200

    Update scripting.py

--- scripting.py
+++ scripting.py
@@ -1,11 +1,10 @@
 import base64
 
 #Open file
-with open('b64.txt') as f:
-    msg = f.read()
+words = open("b64.txt","r").read();
 
 #Decode 50 times
-for _ in range(50):
-    msg = base64.b64decode(msg)
+for i in range(50):
+    words = base64.b64decode(words)
 
-print(f"The flag is: {msg.decode('utf8')}")
+print(f"The flag is: {words.decode('utf8')}")
